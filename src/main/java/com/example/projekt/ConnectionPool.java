0fa03f2c4d6e3294d47746a5e43909042c86c109
konrad2501 <konmic98@gmail.com>
2020-07-11 09:01:04 +0200

    update files

--- src/main/java/com/example/projekt/ConnectionPool.java
+++ src/main/java/com/example/projekt/ConnectionPool.java
@@ -13,25 +13,25 @@ public class ConnectionPool {
 
     Connection conn = null;
     public Connection getConn() {
-        try
-        {
-            //Class.forName("org.postgresql.Driver");
-            conn =  DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "password");
-            System.out.println("Connected");
-            //return conn;
-        } catch (SQLException e1) {
-            e1.printStackTrace();
-        }
-//          String username = "gdqekcfkewuhhb";
-//          String password = "029f2feb66405dad1edab866a835ed398b857a2954c89e895cec6bc8cd1e862d";
-//          String dbUrl = "jdbc:postgresql://" + "ec2-54-75-244-161.eu-west-1.compute.amazonaws.com:5432"
-//                + "/d15rkr8aga0lvv";  //"/d15rkr8aga0lvv?sslmode=require";
-
-//        try {
-//            return DriverManager.getConnection(dbUrl, username, password);
-//        } catch (SQLException e) {
-//            e.printStackTrace();
+//        try
+//        {
+//            //Class.forName("org.postgresql.Driver");
+//            conn =  DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "password");
+//            System.out.println("Connected");
+//            //return conn;
+//        } catch (SQLException e1) {
+//            e1.printStackTrace();
 //        }
+          String username = "gdqekcfkewuhhb";
+          String password = "029f2feb66405dad1edab866a835ed398b857a2954c89e895cec6bc8cd1e862d";
+          String dbUrl = "jdbc:postgresql://" + "ec2-54-75-244-161.eu-west-1.compute.amazonaws.com:5432"
+                + "/d15rkr8aga0lvv";  //"/d15rkr8aga0lvv?sslmode=require";
+
+        try {
+            return DriverManager.getConnection(dbUrl, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return conn;
     }
 
